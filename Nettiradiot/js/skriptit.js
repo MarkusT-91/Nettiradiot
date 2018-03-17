d8eb26ae7dde79ec92c43191590a6ea42bbbad1d
Markus Tuominen <markus.tuominen91@gmail.com>
2018-03-17 22:59:58 +0200

    fontawesome icons

--- Nettiradiot/js/skriptit.js
+++ Nettiradiot/js/skriptit.js
@@ -1,59 +1,69 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     var helmi = document.getElementById("audio_helmi");
     var bluesrocklegends = document.getElementById("audio_bluesrocklegends");
     var bluesbaby = document.getElementById("audio_bluesbaby");
 
-    $("#nappi_helmi").click(function(){
-        
+    $("#nappi_helmi").click(function () {
+
         bluesrocklegends.pause();
-        $("#nappikuva_bluesrocklegends").attr("src","img/play.png");
+        $("#nappi_bluesrocklegends").find('i').attr('class', 'fa fa-play-circle fa-10x');
         bluesbaby.pause();
-        $("#nappikuva_bluesbaby").attr("src","img/play.png");
+        $("#nappi_bluesbaby").find('i').attr('class', 'fa fa-play-circle fa-10x');
 
 
         helmi.paused ? helmi.play() : helmi.pause();
 
-        if(helmi.play) {
-            $("#nappikuva_helmi").attr("src","img/pause.png");
+        //Vaihdetaan kuvake sen mukaan soiko radio vai ei
+        if (helmi.play) {
+            $(this).find('i').attr('class', 'fa fa-pause-circle fa-10x');
         }
         if (helmi.paused) {
-            $("#nappikuva_helmi").attr("src","img/play.png");
+            $(this).find('i').attr('class', 'fa fa-play-circle fa-10x');
         }
     });
 
-    $("#nappi_bluesrocklegends").click(function(){
-        
+    $("#nappi_bluesrocklegends").click(function () {
+
         bluesbaby.pause();
-        $("#nappikuva_bluesbaby").attr("src","img/play.png");
+        $("#nappi_bluesbaby").find('i').attr('class', 'fa fa-play-circle fa-10x');
         helmi.pause();
-        $("#nappikuva_helmi").attr("src","img/play.png");
+        $("#nappi_helmi").find('i').attr('class', 'fa fa-play-circle fa-10x');
 
         bluesrocklegends.paused ? bluesrocklegends.play() : bluesrocklegends.pause();
 
-        if(bluesrocklegends.play) {
-            $("#nappikuva_bluesrocklegends").attr("src","img/pause.png");
+        if (bluesrocklegends.play) {
+            $(this).find('i').attr('class', 'fa fa-pause-circle fa-10x');
         }
-        if (bluesrocklegends.paused) {           
-            $("#nappikuva_bluesrocklegends").attr("src","img/play.png");
+        if (bluesrocklegends.paused) {
+            $(this).find('i').attr('class', 'fa fa-play-circle fa-10x');
         }
     });
 
-    $("#nappi_bluesbaby").click(function(){
-        
+    $("#nappi_bluesbaby").click(function () {
+
         helmi.pause();
-        $("#nappikuva_helmi").attr("src","img/play.png");
+        $("#nappi_helmi").find('i').attr('class', 'fa fa-play-circle fa-10x');
         bluesrocklegends.pause();
-        $("#nappikuva_bluesrocklegends").attr("src","img/play.png");
+        $("#nappi_bluesrocklegends").find('i').attr('class', 'fa fa-play-circle fa-10x');
 
         bluesbaby.paused ? bluesbaby.play() : bluesbaby.pause();
 
-        if(bluesbaby.play) {
-            $("#nappikuva_bluesbaby").attr("src","img/pause.png");
+        if (bluesbaby.play) {
+            $(this).find('i').attr('class', 'fa fa-pause-circle fa-10x');
         }
         if (bluesbaby.paused) {
-            $("#nappikuva_bluesbaby").attr("src","img/play.png");
+            $(this).find('i').attr('class', 'fa fa-play-circle fa-10x');
         }
     });
+
+    // function kuvakeVaihtuu(mikakanava) {
+    //     if(mikakanava.play) {
+    //         $(this).find('i').attr('class', 'fa fa-pause-circle fa-10x');
+    //     }
+    //     if (mikakanava.paused) {
+    //         $(this).find('i').attr('class', 'fa fa-play-circle fa-10x');
+    //     }
+    // }
 
 });
